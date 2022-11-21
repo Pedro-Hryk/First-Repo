3b782b7295849961c15e0d699ba56cf54155bc1d
Pedro-Hryk <vidz0679@gmail.com>
2022-11-21 00:01:40 -0300

    yes

---
+++ HeyoJavaScript.js
@@ -0,0 +1,61 @@
+let classRoom = [
+    {
+        "Marnie" : [
+                {"Monday" : true},
+                {"Tuesday" : true},
+                {"Wednesday" : true},
+                {"Thursday" : true},
+                {"Friday" : true}
+            ]
+    },
+    {
+        "Lena" : [
+                {"Monday" : false},
+                {"Tuesday" : false},
+                {"Wednesday" : true},
+                {"Thursday" : false},
+                {"Friday" : true}
+            ]
+    },
+    {
+        "Shoshanna" : [
+                {"Monday" : true},
+                {"Tuesday" : true},
+                {"Wednesday" : false},
+                {"Thursday" : true},
+                {"Friday" : false}
+            ]
+    },
+    {
+        "Jessa" : [
+                {"Monday" : false},
+                {"Tuesday" : false},
+                {"Wednesday" : false},
+                {"Thursday" : false},
+                {"Friday" : true}
+            ]
+    }
+];
+
+function attendanceCheck(day){
+
+    let students = [];
+    let arr = [];
+    for(let i = 0; i < classRoom.length; i++){
+        
+        let key = Object.keys(classRoom[i]);
+        for(let j = 0; j < classRoom[i][key].length; j++){
+            
+            if(classRoom[i][key][j][day] === true){                
+                students.push(key[0]);
+            }
+        }
+        
+    }
+    return students;
+    
+  
+}
+
+console.log(attendanceCheck('Monday'));
+
